0aa5ab73b40ee5892f94bd12c46176e749c5a2a5
Walter Morales <walterrafael26@gmail.com>
2021-03-11 19:18:18 -0600

    Intialized project for work

---
+++ CollaborativeWork/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CollaborativeWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
