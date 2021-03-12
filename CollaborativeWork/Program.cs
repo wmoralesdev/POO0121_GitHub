f4753328845374cc617a2a917a93562d0a995fda
Walter Morales <walterrafael26@gmail.com>
2021-03-11 19:22:50 -0600

    Assigned tasks

--- CollaborativeWork/Program.cs
+++ CollaborativeWork/Program.cs
@@ -8,5 +8,14 @@ namespace CollaborativeWork
         {
             Console.WriteLine("Hello World!");
         }
+
+        // TODO: Estudiante A -> Crear funcion para leer datos desde teclado
+        
+
+        // TODO: Estudiante B -> Crear una funcion que reciba un string
+        // y verificar si tiene cantidad par de letras (caracteres)
+
+
+        // TODO: Yo -> Crear una funcion para determinar si un numero es par
     }
 }
